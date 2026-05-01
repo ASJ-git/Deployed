9a763a2fe73e5dbe5b864435e6f5c591884eeb91
USER <abrususam@gmail.com>
2026-05-01 16:01:18 +0000

    pagination

---
+++ src/Components/Pagination.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+const Pagination = ({
+  totalPosts,
+  postsPerPage,
+  setCurrentPage,
+  currentPage,
+}) => {
+  let pages = [];
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pages.push(i);
+  }
+  return (
+    <div>
+      {pages.map((page, index) => {
+        return (
+          <button
+            key={index}
+            onClick={() => setCurrentPage(page)}
+            className={`px-1.5 cursor-pointer mx-2 text-white text-2xl ${
+              page === currentPage ? 'bg-blue-500 font-bold' : 'bg-blue-900'
+            }`}
+          >
+            {page}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Pagination;
--- src/Components/Showcase.jsx
+++ src/Components/Showcase.jsx
@@ -1,9 +1,16 @@
 import WebsiteCard from './WebsiteCard';
 import websites from '../Utitlities/Websites.js';
+import { useState } from 'react';
+import Pagination from './Pagination.jsx';
 const Showcase = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(6);
+  const lastPage = currentPage * postsPerPage;
+  const firstPage = lastPage - postsPerPage;
+  const currentPost = websites.slice(firstPage, lastPage);
   return (
-    <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6 w-[90%] max-w-[1366px] mx-auto py-20">
-      {websites.map((website) => (
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6 w-[90%] max-w-[1366px] mx-auto pb-10 pt-20">
+      {currentPost.map((website) => (
         <WebsiteCard
           key={crypto.randomUUID()}
           src={website.src}
@@ -12,6 +19,14 @@ const Showcase = () => {
           websiteLink={website.websiteLink}
         />
       ))}
+      <div className="lg:col-span-3 md:col-span-2 col-span-1 flex justify-center">
+        <Pagination
+          totalPosts={websites.length}
+          postsPerPage={postsPerPage}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        />
+      </div>
     </div>
   );
 };
